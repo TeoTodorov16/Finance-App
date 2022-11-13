575df9a5a9f43eee1e91924463b20c9c6de29dd7
Teodor Todorov <WorkCoding@Teodors-MacBook-Pro.local>
2022-11-13 12:43:15 +0200

    Created, Cleaned Up some default unnecessary junk and SetUp

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Example from './components/Example';
+
+function App() {
+  return (
+    <div>
+      <Example></Example>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Example.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './example.css';
+
+const Example = () => {
+    return (
+        <div>
+            Wassup
+        </div>
+    )
+}
+
+export default Example
