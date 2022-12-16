5d198eea626c8673e6a27e85534b587b277842b6
austBrink <austbrink@gmail.com>
2022-12-16 06:00:13 -0600

    edit options cards

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -5,22 +5,14 @@ import {
     Divider
 } from '@mui/material';
 import UserContext from '../../context/UserContext';
-import { CreateCategory } from '../CreateCategory/CreateCategory';
 import { ListCategories } from '../ListCategories/ListCategories';
 
 export function Dashboard (props) {
 
     const { user } = useContext(UserContext);
 
-    useEffect(() => {
-        console.log(`this is the user from context from crud: ${user.userID}`);
-        // createCategory(user.userID);
-    },[]);
-
     return (
         <Box>
-            <CreateCategory />
-            <Divider />
             <ListCategories />
         </Box>
     )
--- src/components/ListCategories/ListCategories.js
+++ src/components/ListCategories/ListCategories.js
@@ -1,10 +1,8 @@
 import react, { useEffect, useState, useContext } from 'react';
-
 import {
     getRef,
     getOnValue,
 } from '../../utils/firebase';
-
 import {
     Box,
     Paper,
@@ -16,11 +14,16 @@ import {
     Grid,
     CardActionArea,
     Fab,
-    Tooltip
+    Tooltip,
+    IconButton,
 } from '@mui/material';
-
 import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
 import UserContext from '../../context/UserContext';
+import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+
+import { CreateCategory } from '../CreateCategory/CreateCategory';
 import { formatter } from '../../utils/genUtils';
 
 export const ListCategories = () => {
@@ -32,6 +35,7 @@ export const ListCategories = () => {
     // UI State 
     const [ cardHover, setCardHover ] = useState(null);
     const [ cardClicked, setCardClicked ] = useState(null);
+    const [ editorDialogOpen, setEditorDialogOpen ] = useState(false);
 
     // FUNCTIONS // 
 
@@ -66,7 +70,6 @@ export const ListCategories = () => {
         <Box sx = {{
             padding: '10px',
         }}>
-            
             <Divider>
                 <Box sx = {{
                     margin: '0px 15px 0px 15px',
@@ -75,13 +78,16 @@ export const ListCategories = () => {
                     justifyContent: 'center',
                     alignItems: 'center'
                 }}>
-                    <Typography variant = 'h3'>
-                        Your Categories
+                    <Typography variant = 'h4' sx = {{
+                        letterSpacing: '5px'
+                    }}>
+                        YOUR CATEGORIES
                     </Typography>
                     <Fab 
                         size = {'small'}
                         color="primary"
                         aria-label="add"
+                        sx ={{'&:hover': {transform: 'scale(1.2)'}}}
                     >
                         <AddIcon />
                     </Fab>
@@ -108,12 +114,14 @@ export const ListCategories = () => {
                                             width: '220px',
                                             display: 'flex',
                                             flexDirection: 'column',
+                                            cursor: 'pointer',
                                         } : {
                                             height: '200px',
                                             width: '200px',
                                             display: 'flex',
-                                            flexDirection: 'column'
-                                    }}>
+                                            flexDirection: 'column',
+                                            cursor: 'pointer'
+                                        }}>
                       
                                         <CardContent>
                                             <Typography variant = 'h5'>
@@ -124,13 +132,28 @@ export const ListCategories = () => {
                                                 {formatter.format(x.balance)}
                                             </Typography>   
                                         </CardContent>
+                                        
                                         <Box sx = {{
                                             marginTop: 'auto',
-                                            display: 'flex',
-                                            padding: '10px'
                                         }}>
-                                            { cardClicked === x.id ? 
-                                                <Button>Yo</Button> 
+                                            
+                                            { cardHover === x.id ? 
+                                                <Box sx = {{
+                                                    display: 'flex',
+                                                    padding: '0px 10px 5px 10px'
+                                                }}>
+                                                    <IconButton>
+                                                        <SwapHorizIcon />
+                                                    </IconButton>
+                                                    <IconButton>
+                                                        <EditIcon />
+                                                    </IconButton>
+                                                    <IconButton sx = {{
+                                                        marginLeft: 'auto'
+                                                    }}>
+                                                        <DeleteForeverIcon color= 'error'/>
+                                                    </IconButton>
+                                                </Box>  
                                             : 
                                                 <></> 
                                             }
